85f11645347f1f02c5dd21c41858ecce5be6f9f8
ScottSavarie <scottsavarie@gmail.com>
2014-06-11 11:37:16 -0400

    Initial Commit

---
+++ week-1-facebook-newsfeed/Facebook Newsfeed/Facebook Newsfeed/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Facebook Newsfeed
+//
+//  Created by Scott Savarie on 6/11/14.
+//  Copyright (c) 2014 Scott Savarie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ week-1-facebook-newsfeed/Facebook Newsfeed/Facebook NewsfeedTests/Facebook_NewsfeedTests.m
@@ -0,0 +1,34 @@
+//
+//  Facebook_NewsfeedTests.m
+//  Facebook NewsfeedTests
+//
+//  Created by Scott Savarie on 6/11/14.
+//  Copyright (c) 2014 Scott Savarie. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Facebook_NewsfeedTests : XCTestCase
+
+@end
+
+@implementation Facebook_NewsfeedTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
